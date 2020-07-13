815635eaea0bd3642d845832129a969c20130378
John VanDeWeghe <vandeweghe@lunixlabs.com>
2020-07-12 23:54:28 -0500

    initial commit

---
+++ src/interfaces.ts
@@ -0,0 +1,41 @@
+export interface Event {
+    type: string;
+    eventGroup?: Group;
+    messageEventData?: any;
+    commandEventData?: any;
+    readonly datetime?: Date;
+    readonly user?: User;
+    id?: string;
+}
+
+export interface Group {
+    name?: string;
+    description?: string;
+    discoverable?: boolean;
+    owner?: Group;
+    id?: string;
+}
+
+export interface GroupMember {
+    user?: User;
+    userGroup?: Group;
+    lastSeenEvent?: Event;
+    id?: string;
+}
+
+export interface Subscription {
+    transport: string;
+    eventTypes?: string[];
+    groupMember?: GroupMember;
+    webhookData?: any;
+    id?: string;
+}
+
+export interface User {
+    name?: string;
+    email?: string;
+    picture?: string;
+    nickname?: string;
+    id?: string;
+}
+
